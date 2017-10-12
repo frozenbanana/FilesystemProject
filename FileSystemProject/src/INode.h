3e7be53a231519114fefae75cae9d4779c0eaf79
Oliver Glandberger <oliverglandberger@gmail.com>
2017-10-12 14:10:32 +0000

    Alpha version of INode and INodeManager, ready for testing

--- FileSystemProject/src/INode.h
+++ FileSystemProject/src/INode.h
@@ -8,42 +8,48 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include "block.h"
 
-class DataBlockHandle
+class BlockHandle
 {
   private:
-    int		*m_thisDataBlock;   // Data type STILL TO BE DETERMINED
+    Block		*m_thisBlock;
 
   public:
-    DataBlockHandle();
-    ~DataBlockHandle();
+    BlockHandle();
+    ~BlockHandle();
 
-	int*	GetDataBlock();
-	void	SetDataBlock(int *dataBlock_in);
+  	Block*	GetBlock();
+  	void	  SetBlock(int *Block_in);
 
-	DataBlockHandle      *m_nextDataHandle;
+	  BlockHandle      *m_nextHandle; // Public in-case someone else needs to 'step'
 };
 
 class INode
 {
   private:
-    bool				m_isDirectory;
-    DataBlockHandle		m_rootDataHandle;
-    bool				m_permissionData[10];
+    bool	      			m_isDirectory;
+    bool			      	m_permissionData[10];
 
-	void				CleanAll();
-    void				m_CleanNextDataHandle(DataBlockHandle *DataBlockHandle_in);
+	  void				CleanAll();
+    void				CleanNextHandle(BlockHandle *BlockHandle_in);
+
+    void        ExtendList(BlockHandle* rootHandle_in, int blockCount_in);
+    void        InsertList(BlockHandle* rootHandle_in, Block* *BlockArray_in, int BlockCount_in);
 
   public:
     INode();
     INode(bool isDirectory_in, bool *permissionData_in);
     ~INode();
 
-	/*- - - - - FUNCTION COMMENTS - - - - -
+    BlockHandle   		m_rootHandle;     // rootHandle public to gain access
+
+	/*- - - - - FUNCTION COMMENTS - - - - -   // NEED TO UPDATE THIS DESCRIPTION
+   * LAST
 	 • Extends the linked list within the INode
-	 • RECAP: Members of linked list point to 1 DataBlock each
-	 • RETURN VALUE: Address of new 'DataBlockHandle' member*/
-	DataBlockHandle* ExtendHandleList();
+	 • RECAP: Members of linked list point to 1 Block each
+	 • RETURN VALUE: Address of new 'BlockHandle' member*/
+	  bool  InsertBlocks(int BlockCount_in, Block* *BlockArray_in);
 };
 
 #endif
